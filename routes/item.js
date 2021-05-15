dd6dd1071830602f0a51820e7059a309be734488
Hela <hela.chatti.landolsi@gmail.com>
2021-05-15 20:34:20 +0200

    mern app

---
+++ routes/item.js
@@ -0,0 +1,37 @@
+const express = require ("express");
+const router = express.Router();
+
+const Item = require("../modules/Item");
+
+
+
+router.get('/', (req, res) => {
+    Item.find()
+    .then((items) => {
+        console.log("hi from get methood");
+        res.send(items)})
+});
+
+router.post('/', (req, res) => {
+    const newItem = new Item({
+        name: req.body.name,
+    })
+    newItem.save()
+    .then((item) => {
+        console.log("hello from post method");
+        res.send(item)})
+})
+router.delete('/:id', (req,res) => {
+    Item.findById(req.params.id)
+    .then((deleteditem ) => deleteditem.remove().then((Item) => res.send(Item)))
+    .catch((error) => res.status(404).json({success:false}))
+})
+
+
+
+
+
+module.exports = router;
+
+
+
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const db = require ('./config/keys').mongoURI;
+
+
+const items = require("./routes/item");
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+//use Routes
+app.use('/items',items);
+
+if(process.nextTick.NODE_ENV === "production"){
+  app.use(express.static("client/build"));
+  app.get('*',(req,res) => {
+   res.sendFile(require('path').resolve(__dirname, 'client', 'build' , 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+//connect to mongodb
+mongoose
+.connect(db, {useNewUrlParser: true, useUnifiedTopology: true })
+.then (()=> console.log("connect to mongoDB"))
+.catch((error) => console.log(`an error has occured : ${error}`))
+
+
+app.all('*', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://localhost:3000");
+    next();
+});
+//run server
+app.listen (PORT,()=> console.log(`listening to server on port ${PORT}`))

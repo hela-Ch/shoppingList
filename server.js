37845c8bbc6471357a406959ab1825361016b567
Hela <hela.chatti.landolsi@gmail.com>
2021-05-16 14:38:31 +0200

    gfhjklmjhgf

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongoose
 
 
 if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.resolve(_dirname,"client/build")));
+  app.use(express.static(path.resolve(__dirname,"client/build")));
   app.get('*',(req,res) => {
    res.sendFile(path.resolve(__dirname, 'client', 'build' , 'index.html'));
   });
